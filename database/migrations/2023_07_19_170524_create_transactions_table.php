65cde411e2de3c57fe21c8047f26271776ea7352
Yoga Andrian <yogaandrianp21@gmail.com>
2023-07-20 15:07:59 +0700

    create migration - table sellers, categories, products, galleries, shopping_carts, transactions

---
+++ database/migrations/2023_07_19_170524_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('transaction_code')->unique();
+            $table->foreignId('user_id');
+            $table->string('address');
+            $table->string('shipping_price');
+            $table->string('price_amount');
+            $table->string('total_payment');
+            $table->string('payment_method');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
